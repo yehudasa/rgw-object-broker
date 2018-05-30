d709f018bb59be447f280b10b5a2fd98fa81f62f
Yehuda Sadeh <yehuda@redhat.com>
2018-05-29 18:03:53 -0700

    rgw-broker: create a new user for each instance
    and create new bucket under the new user. Update bindings accordingly
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -20,24 +20,30 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
+	"bytes"
+	"io/ioutil"
+	"encoding/json"
 	"github.com/golang/glog"
 	"github.com/rs/xid"
-	// "io/ioutil"
-	// "net/http"
+	"net/http"
+	"net/url"
 	"sync"
 
 	"github.com/kubernetes-incubator/service-catalog/pkg/brokerapi"
 	"github.com/minio/minio-go"
+	"github.com/minio/minio-go/pkg/s3signer"
         // metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	k8sRest "k8s.io/client-go/rest"
 )
 
 const (
+	USER_NAME       = "userName"
 	BUCKET_ENDPOINT = "bucketEndpoint"
 	BUCKET_NAME     = "bucketName"
-	BUCKET_ID       = "bucketID"
-	BUCKET_PASS	= "bucketPass"
+	ACCESS_KEY      = "accessKey"
+	SECRET_KEY	= "secretKey"
 )
 
 type s3ServiceInstance struct {
@@ -47,22 +53,63 @@ type s3ServiceInstance struct {
 	Credential brokerapi.Credential // s3 server url, includes port and bucket name
 }
 
+type RGWUser struct {
+        name            string
+        accessKey       string
+        secret          string
+}
+
+type RGWClient struct {
+        endpoint        string
+        user            RGWUser
+        client          *minio.Client
+}
+
+func (c *RGWClient) init() error {
+        client, err := getS3Client(c.user, c.endpoint)
+
+        if err != nil {
+                return fmt.Errorf("getS3Client failed: %v", err)
+        }
+        c.client = client
+        return nil
+}
+
+// Creates an bucket
+func (c *RGWClient) createBucket(bucketName string) error {
+	glog.Infof("Creating bucket %q", bucketName)
+	location := "" // ignored for now...
+	// create new bucket
+	err := c.client.MakeBucket(bucketName, location)
+	if err != nil {
+		glog.Errorf("Error creating bucket: %v", err)
+		return fmt.Errorf("S3-Client.MakeBucket err: %v", err)
+	}
+	glog.Infof("Create bucket %q succeeded.", bucketName)
+	return nil
+}
+
 type broker struct {
 	// rwMutex controls concurrent R and RW access
 	rwMutex	    sync.RWMutex
 	// instanceMap maps instanceIDs to the ID's userProvidedServiceInstance values
 	instanceMap map[string]*s3ServiceInstance
-	// client used to access s3 API
-	s3Client    *minio.Client
+
+        rgw         RGWClient
+
 	// s3 server ip and port
-	s3url	    string // includes ":port"
-	s3ID	    string
-	s3Pass	    string
+	uidPrefix   string
 	// client used to access kubernetes
 	kubeClient  *clientset.Clientset
 }
 
-// Initialize the s3-gluster service broker. This function is called by `server.Start()`.
+
+type bucketInstance struct {
+        user            RGWUser
+        bucketName      string
+}
+
+// Initialize the rgw service broker. This function is called by `server.Start()`.
 func CreateBroker() Broker {
 	var instanceMap = make(map[string]*s3ServiceInstance)
 	glog.Info("Generating new s3 broker.")
@@ -73,34 +120,35 @@ func CreateBroker() Broker {
 		glog.Fatalf("failed to get kubernetes client: %v\n", err)
 	}
 
-        s3endpoint, user, pass := "", "", ""
+        client := RGWClient{}
+        uidPrefix := "kube-rgw."
 
         for _, e := range os.Environ() {
                 pair := strings.Split(e, "=")
                 switch pair[0] {
 		case "S3_ENDPOINT":
-                        s3endpoint = pair[1]
+                        client.endpoint = pair[1]
 		case "S3_ACCESS_KEY":
-                        user = pair[1]
+                        client.user.accessKey = pair[1]
 		case "S3_SECRET":
-                        pass = pair[1]
+                        client.user.secret = pair[1]
+		case "RGW_UID_PREFIX":
+                        uidPrefix = pair[1]
 		}
         }
 
 	// get the s3 client
-	s3c, err := getS3Client(user, pass, s3endpoint)
+	err = client.init()
 	if err != nil {
-		glog.Fatalf("failed to get minio-s3 client: %v\n", err)
+		glog.Fatalf("failed to get s3 client: %v\n", err)
 	}
 
-	glog.Infof("New Broker for s3 endpoint: %s", s3endpoint)
+	glog.Infof("New Broker for s3 endpoint: %s", client.endpoint)
 	return &broker{
 		instanceMap: instanceMap,
-		s3Client:    s3c,
-		s3url:       s3endpoint,
+		rgw:         client,
 		kubeClient:  cs,
-		s3ID:        user,
-		s3Pass:      pass,
+		uidPrefix:   uidPrefix,
 	}
 }
 // Implements the `Catalog` interface method.
@@ -150,25 +198,37 @@ func (b *broker) CreateServiceInstance(instanceID string, req *brokerapi.CreateS
 		bucketName = xid.New().String()
 	}
 	glog.Infof("Creating new bucket: %q for instance %q.", bucketName, instanceID)
+
 	// create new service instance
-	exists, err := b.checkBucketExists(bucketName)
-	if err != nil {
-		return nil, fmt.Errorf("Error occured while checking bucket name availability.")
-	}
-	if exists {
-		glog.Errorf("Bucket name unavailable: %q already exists.", bucketName)
-		return nil, fmt.Errorf("Bucket name unavailable: %q already exists.", bucketName)
-	}
-	if err := b.provisionBucket(bucketName); err != nil {
+
+        userName := b.uidPrefix + xid.New().String()
+
+        // First create a new user
+        newUser, err := b.provisionUser(userName, instanceID)
+        if err != nil {
+                return nil, err
+        }
+
+        newClient := RGWClient{}
+        newClient.user = *newUser
+        newClient.endpoint = b.rgw.endpoint
+        err = newClient.init()
+        if err != nil {
+                glog.Errorf("Failed to init s3 client for new user: %v", err)
+		return nil, fmt.Errorf("Failed to init s3 client for new user: %v", err)
+        }
+
+	if err := newClient.createBucket(bucketName); err != nil {
 		return nil, err
 	}
 	b.instanceMap[instanceID] = &s3ServiceInstance{
 		Namespace: req.ContextProfile.Namespace,
 		Credential: brokerapi.Credential{
+			USER_NAME:       newUser.name,
 			BUCKET_NAME:     bucketName,
-			BUCKET_ENDPOINT: b.s3url,
-			BUCKET_ID:       b.s3ID,
-			BUCKET_PASS:	 b.s3Pass,
+			BUCKET_ENDPOINT: newClient.endpoint,
+                        ACCESS_KEY:      newUser.accessKey,
+			SECRET_KEY:	 newUser.secret,
 		},
 	}
 	return nil, nil
@@ -191,7 +251,7 @@ func (b *broker) RemoveServiceInstance(instanceID, serviceID, planID string, acc
 	}
 	if exists {
 		glog.Infof("Removing bucket %q", bucketName)
-		if err := b.s3Client.RemoveBucket(bucketName); err != nil {
+		if err := b.rgw.client.RemoveBucket(bucketName); err != nil {
 			glog.Errorf("Error during RemoveBucket: %v", err)
 			return nil, fmt.Errorf("S3 Client errored while removing bucket: %v", err)
 		}
@@ -226,26 +286,112 @@ func (b *broker) UnBind(instanceID, bindingID, serviceID, planID string) error {
 	return nil
 }
 
-// Creates an s3 compatible bucket of the passed-in name.
-func (b *broker) provisionBucket(bucketName string) error {
-	glog.Infof("Creating bucket %q", bucketName)
-	location := "" // ignored for now...
-	// create new bucket
-	err := b.s3Client.MakeBucket(bucketName, location)
-	if err != nil {
-		glog.Errorf("Error creating bucket: %v", err)
-		return fmt.Errorf("S3-Client.MakeBucket err: %v", err)
+func (b *broker) rgwAdminRequest(method, section string, params url.Values) ([]byte, error) {
+	httpClient := &http.Client{
+		Timeout:   30 * time.Second,
+		Transport: http.DefaultTransport,
 	}
-	glog.Infof("Create bucket %q succeeded.", bucketName)
-	return nil
+
+        req_url := b.rgw.endpoint + "/admin/" + section + "?" + params.Encode()
+        glog.Infof("sending http request: %s", req_url)
+
+	req, err := http.NewRequest(method, req_url, bytes.NewReader(nil))
+	if err != nil {
+		glog.Errorf("Error creating http request params=%v", params)
+		return nil, fmt.Errorf("Error creating http request params=%v", params)
+	}
+
+        req = s3signer.SignV4(*req, b.rgw.user.accessKey, b.rgw.user.secret, "", "")
+	if req.Header.Get("Authorization") == "" {
+		glog.Errorf("Error signing request: Authorization header is missing")
+		return nil, fmt.Errorf("Error signing request: Authorization header is missing")
+	}
+
+        resp, err := httpClient.Do(req)
+	if err != nil {
+		glog.Errorf("Error sending http request: %v", err)
+		return nil, fmt.Errorf("httpClient.Do err: %v", err)
+	}
+
+        defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+                glog.Errorf("Error got http resonse: %v", resp.StatusCode)
+                return nil, fmt.Errorf("Error got http response: %v", resp.StatusCode)
+	}
+
+        body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		glog.Errorf("Error reading response: %v", err)
+		return nil, fmt.Errorf("Error reading response: %v", err)
+	}
+
+        return body, nil
 }
+
+
+func (b *broker) provisionUser(userName, bindingID string) (*RGWUser, error) {
+	glog.Infof("Creating user %q", userName)
+
+	// Set request parameters.
+	params := make(url.Values)
+	params.Set("uid", userName)
+        params.Set("display-name", "rgw-broker-"+bindingID)
+        params.Set("key-type", "s3")
+        params.Set("generate-key", "true")
+
+        _, err := b.rgwAdminRequest("PUT", "user", params)
+	if err != nil {
+		glog.Errorf("Error creating user: %v", err)
+		return nil, fmt.Errorf("Error creating user: %v", err)
+	}
+
+        params = make(url.Values)
+	params.Set("uid", userName)
+        body, err := b.rgwAdminRequest("GET", "user", params)
+	if err != nil {
+		glog.Errorf("Error fetching user info: %v", err)
+                return nil, fmt.Errorf("Error fetching user info: %v", err)
+	}
+
+        type userInfo struct {
+                UserId  string    `json:"user_id"`
+                Keys    []struct {
+                        AccessKey string  `json:"access_key"`
+                        Secret string     `json:"secret_key"`
+                } `json:"keys"`
+        }
+
+        res := userInfo{}
+        err = json.Unmarshal(body, &res)
+        if (err != nil) {
+                glog.Errorf("Error failed to unmarshal user info: %v", err)
+                return nil, fmt.Errorf("Error failed to unmarshal user info: %v", err)
+        }
+
+        if len(res.Keys) < 1 {
+                glog.Errorf("Error access key wasn't generated for user %s", userName)
+                return nil, fmt.Errorf("Error access key wasn't generated for user %s", userName)
+        }
+
+        glog.Infof("generated user %s (access_key=%s)", userName, res.Keys[0].AccessKey)
+
+        user := new(RGWUser)
+        user.name = userName
+        user.accessKey = res.Keys[0].AccessKey
+        user.secret = res.Keys[0].Secret
+
+        return user, nil
+}
+
+
 
 // Returns true if the passed-in bucket exists.
 // TODO: long way of checking for bucket name collision. gluster-swift does not support
 //   the api call that minio.BucketExists() maps to; always fails with "400 bad request".
 func (b *broker) checkBucketExists(bucketName string) (bool, error) {
 	glog.Infof("Checking if bucket name %q already exists.", bucketName)
-	buckets, err := b.s3Client.ListBuckets()
+	buckets, err := b.rgw.client.ListBuckets()
 	if err != nil {
 		glog.Errorf("Error occurred during ListBuckets: %v", err)
 		return false, fmt.Errorf("Error occurred during ListBuckets: %v", err)
@@ -261,8 +407,8 @@ func (b *broker) checkBucketExists(bucketName string) (bool, error) {
 }
 
 // Returns a minio api client.
-func getS3Client(user, pass, endpoint string) (*minio.Client, error) {
-	glog.Infof("Creating s3 client based on: \"%s\" on endpoint %s", user, endpoint)
+func getS3Client(user RGWUser, endpoint string) (*minio.Client, error) {
+	glog.Infof("Creating s3 client based on: \"%s\" on endpoint %s", user.accessKey, endpoint)
 
         addr := endpoint
         useSSL := false
@@ -277,10 +423,11 @@ func getS3Client(user, pass, endpoint string) (*minio.Client, error) {
 
         glog.Infof("  addr=%s (ssl=%t)", addr, useSSL)
 
-	minioClient, err := minio.NewV2(addr, user, pass, useSSL)
+	minioClient, err := minio.NewV2(addr, user.accessKey, user.secret, useSSL)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to create S3 client instance: %v", err)
 	}
+
 	return minioClient, nil
 }
 
